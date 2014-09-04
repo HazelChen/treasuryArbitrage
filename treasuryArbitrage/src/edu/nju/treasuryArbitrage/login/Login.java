25875138ee9cc7c18018a4292ec81586ae5fef29
Hazel <cfzljcl@gmail.com>
2014-09-04 23:17:19 +0800

    login

---
+++ treasuryArbitrage/src/edu/nju/treasuryArbitrage/login/Login.java
@@ -0,0 +1,21 @@
+package edu.nju.treasuryArbitrage.login;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class Login extends JPanel{
+	public static final String TITLE = "国债期货跨期交易系统";
+	
+	public Login() {
+		
+	}
+	
+	public void init() {
+		this.setBackground(Color.BLACK);
+	}
+	
+	public void assemble() {
+		
+	}
+}
