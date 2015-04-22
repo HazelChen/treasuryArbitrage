7c5d40fcd4ac05533a1e2a473cee6d073eb54828
Lin <linchen7@foxmail.com>
2015-04-22 15:47:33 +0800

    Fix garbled.

--- treasuryArbitrage/src/edu/nju/treasuryArbitrage/view/common/TextConstants.java
+++ treasuryArbitrage/src/edu/nju/treasuryArbitrage/view/common/TextConstants.java
@@ -2,12 +2,5 @@ package edu.nju.treasuryArbitrage.view.common;
 
 public class TextConstants {
 
-	public static final String TITLE_CHS = "锟斤拷债锟节伙拷锟斤拷锟节斤拷锟斤拷系统";
-	public static final String TITLE_ENG = "The Arbitrage System For Treasury Future";
-	
-	public static final String NAVIGATER1 = "锟节伙拷锟斤拷锟斤拷";
-	public static final String NAVIGATER2 = "锟斤拷锟斤拷锟斤拷锟�";
-	public static final String NAVIGATER3 = "锟街诧拷锟斤拷锟�";
-	public static final String NAVIGATER4 = "锟狡撅拷要锟斤拷";
-	public static final String NAVIGATER5 = "锟斤拷锟斤拷锟斤拷锟斤拷";
+	public static final String TITLE_CHS = "国债期货跨期交易系统";
 }
